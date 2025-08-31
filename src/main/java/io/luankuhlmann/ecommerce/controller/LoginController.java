72ce9e3807fa76c84f6cf6e93992b8e881a0b3d3
LuanKuhlmann <l.campos.kuhlmann@outlook.com>
2025-08-31 20:16:08 -0300

    feat: implementado metodo de login no LoginController

--- src/main/java/io/luankuhlmann/ecommerce/controller/LoginController.java
+++ src/main/java/io/luankuhlmann/ecommerce/controller/LoginController.java
@@ -1,6 +1,13 @@
 package io.luankuhlmann.ecommerce.controller;
 
+import io.luankuhlmann.ecommerce.dto.request.LoginRequest;
+import io.luankuhlmann.ecommerce.dto.response.LoginResponse;
+import io.luankuhlmann.ecommerce.service.LoginService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class LoginController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+        LoginResponse response = loginService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
 }
